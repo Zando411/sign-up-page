69e2a01b20209cbfe8ea47c00325929f02f675f6
Zando411 <123271994+Zando411@users.noreply.github.com>
2023-03-15 13:09:15 -0700

    add password match validation

---
+++ app.js
@@ -0,0 +1,14 @@
+function checkPass() {
+  let pass = document.getElementById("user_password").value;
+  let confirmPass = document.getElementById("confirm_password").value;
+
+  console.log(pass, confirmPass);
+  let message = document.getElementById("passwordCheck");
+
+  if (pass.length != 0 && pass == confirmPass) {
+    message.innerHTML = "They match!";
+  } else {
+    message.innerHTML = "Password doesn't match!";
+    message.style.color = "red";
+  }
+}
